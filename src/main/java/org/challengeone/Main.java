945800abcf683f9a257dabb27868dab980a147ee
Julian Gaitan <julian89net@gmail.com>
2024-05-22 12:52:43 -0500

    Refactoring Main to do the currency conversion in API_Response record

--- src/main/java/org/challengeone/Main.java
+++ src/main/java/org/challengeone/Main.java
@@ -1,44 +1,9 @@
 package org.challengeone;
 
-import com.google.gson.Gson;
-import org.challengeone.models.API_Response;
-import org.challengeone.models.CurrencyInfo;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
 
-        System.out.println(CurrencyInfo.CURRENCY_LIST.toString());
-        String API_KEY = "";
-        try {
-            File myFile = new File(".APIKEY");
-            Scanner myReader = new Scanner(myFile);
-            if (myReader.hasNextLine()) {
-                API_KEY = myReader.nextLine();
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
 
-        URI url = URI.create("https://v6.exchangerate-api.com/v6/"+ API_KEY + "/latest/USD");
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(url)
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        API_Response apiResponse = new Gson().fromJson(response.body(), API_Response.class);
-        System.out.println(apiResponse);
     }
 }
--- src/main/java/org/challengeone/models/API_Response.java
+++ src/main/java/org/challengeone/models/API_Response.java
@@ -1,11 +1,56 @@
 package org.challengeone.models;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Map;
+import java.util.Scanner;
 
 public record API_Response(
         String result,
         @SerializedName("error-type") String errorType,
-        String base_code, Map<String, Double> conversion_rates) {
+        String base_code,
+        String target_code,
+        Double conversion_rate,
+        Double conversion_result) {
+
+    private static final String API_KEY = initAPI_KEY();
+
+    private static String initAPI_KEY() {
+        String apiKey = "";
+        try {
+            File myFile = new File(".APIKEY");
+            Scanner myReader = new Scanner(myFile);
+            if (myReader.hasNextLine()) {
+                apiKey = myReader.nextLine();
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return apiKey;
+    }
+
+    public static API_Response doConversion(CurrencyInfo base, CurrencyInfo target, double amount) {
+
+        URI url = URI.create("https://v6.exchangerate-api.com/v6/"+ API_KEY + "/" + base.code() + "/" + target.code() + "/" + amount);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                                         .uri(url)
+                                         .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), API_Response.class);
+        } catch (IOException | InterruptedException e) {
+            return new API_Response("error", e.getMessage(), "", "", 0.0, 0.0);
+        }
+    }
 }
